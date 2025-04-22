93d9e6b6fc6c594827339dbbfdc81041b5967cd9
Arun <140989175+arunisean@users.noreply.github.com>
2025-04-22 14:47:31 +0800

    refactor: Reorganize the codebase

--- causality-graph/p2p/znostr/client-cli/scripts/create-help-manual.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PATH=".:./target/release/:./target/debug/:$PATH" &&
-    nostr-commander-rs --manual >help.manual.txt
-echo "help.manual.txt is $(wc -l help.manual.txt | cut -d ' ' -f1) lines long"
--- causality-graph/p2p/znostr/client-cli/scripts/create-help-usage.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PATH=".:./target/release/:./target/debug/:$PATH" &&
-    nostr-commander-rs --usage >help.usage.txt
-echo "help.usage.txt is $(wc -l help.usage.txt | cut -d ' ' -f1) lines long"
